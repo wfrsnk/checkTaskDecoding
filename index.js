22655a566f9dbfdf994f37d14b283ce022dce1eb
wfrsnk <57827569+wfrsnk@users.noreply.github.com>
2021-10-30 19:58:47 +0300

    a

---
+++ index.js
@@ -0,0 +1,33 @@
+const [{Server: h1}, x] = [require('http'), require('express')];
+const {privateDecrypt: dec} = require('crypto');
+const Busboy = require('busboy');
+
+const Router = x.Router();
+const PORT = 10001;
+
+
+const app = x();
+Router
+    .route('/')
+    .get(r => r.res.end('Привет мир!'))
+    .post(async (req, res) => {
+        console.log(req.headers);
+        let o = {};
+        const boy = new Busboy({headers: req.headers});
+        boy.on('file', (fieldName, file) => file
+            .on('data', data => (o[fieldName] = data))
+            .on('end', () => console.log('File [' + fieldName + '] Finished')));
+        boy.on('finish', () => {
+            let result;
+            try {
+                result = dec(o.key, o.secret);
+            } catch(e){
+                result = 'ERROR';
+            }
+
+        })
+    })
+
+app.listen(PORT, () => {
+console.log(`Server listening at http://localhost:${PORT}`);
+ });

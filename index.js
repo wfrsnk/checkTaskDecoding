10b41e6c448c79fe606a8bfcece57b22788a6586
wfrsnk <57827569+wfrsnk@users.noreply.github.com>
2021-11-06 20:03:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
-const [{Server: h1}, x] = [require('http'), require('express')];
-const {privateDecrypt: dec} = require('crypto');
+const [{ Server: h1 }, x] = [require('http'), require('express')];
+const { privateDecrypt: dec } = require('crypto');
 const Busboy = require('busboy');
 
 const Router = x.Router();
-const PORT = process.env.PORT || 10001;
-const hu = {'Content-Type': 'text/html; charset=utf-8'};
-
+const PORT = 10001;
+/*
+const CORS = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
+    "Access-Control-Allow-Headers":
+    "Content-Type,Accept,Access-Control-Allow-Headers",
+};
+*/
+const { log } = console;
 const app = x();
 Router
-    .route('/')
-    .get(r => r.res.end('Привет мир!'))
-    .post(async (req, res) => {
-        console.log(req.headers);
-        let o = {};
-        const boy = new Busboy({headers: req.headers});
-        boy.on('file', (fieldName, file) => file
-            .on('data', data => (o[fieldName] = data))
-            .on('end', () => console.log('File [' + fieldName + '] Finished')));
-        boy.on('finish', () => {
-            let result;
-            try {
-                result = dec(o.key, o.secret);
-            } catch(e){
-                result = 'ERROR';
-            }  
-            res.send(String(result));
-        });
-       req.pipe(boy);
+  .route('/')
+  .get(r => r.res.end('Привет мир!'))
+  .post(async (req, res) => {
+    console.log(req.headers);
+    let o = {key: '', secret: []};
+    const boy = new Busboy({ headers: req.headers });
+    boy.on('file', (fieldname, file) => file
+      .on('data', data => {
+             if (fieldname == 'key') {
+                 o[fieldname] += data;
+             } else {
+                 o[fieldname].push(data);
+             }
+       }));
+    boy.on('finish', () => {
+      o.secret = Buffer.concat(o.secret);
+      let result;
+      try {
+           result = dec(o.key, o.secret);
+      } catch(e) {
+           result = 'ERROR!';
+      }
+      res
+      /* .set(CORS) */
+      .send(String(result));
     });
-    
-app
-    .use((r, rs, n) => rs.status(200).set(hu) && n())
-    .use('/', Router);
+    req.pipe(boy);
 
-app.listen(PORT, () => {
-console.log(`Server listening at http://localhost:${PORT}`);
- });
+  });
+app
+  .use('/', Router)
+  .use(({ res: r }) => r.status(404).send('Пока нет!'))
+  .use((e, r, rs, n) => rs.status(500).send(`Ошибка: ${e}`))
+  .set('x-powered-by', false);
+module.exports = h1(app)
+  .listen(process.env.PORT || PORT, () => log(process.pid));
